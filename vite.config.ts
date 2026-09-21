5dab96dc827d3f573f343bbfa92821292f3731f9
hackasaur <manikraina1096@gmail.com>
2026-09-21 15:24:36 +0530

    - change base to repo name for github page so that it can find the   assets' path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from "vite";
 export default defineConfig({
   // nothing special needed for a plain TS library playground
   root: ".",
+  base: "/symbolic",
+  build: {
+      outDir: "docs",
+    },
   server: {
     port: 3000,
     open: true,
